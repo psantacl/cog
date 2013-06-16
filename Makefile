e4f690401f35aa23d1109ae40cb9cd25960b033e
Paul Santa Clara <psantaclara@relaynetwork.com>
2013-06-16 15:37:03 -0400

    several new algos for interpretting pipe date as audio floats

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 RUSTC ?= rustc
 
+dummy1 := $(shell mkdir bin 2> /dev/null)
+
 all: src/main.rs
 		$(RUSTC) -o bin/rusty-jack src/main.rs 
 
 clean:
-	rm -f bin/*
+	rm -rf bin/*
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern mod std;
+//extern mod socket;
 
-use core::libc::{c_void, c_int, c_char, c_ulong, c_uint, c_float, c_double, uint32_t, size_t, memset};
-
+use core::libc::{c_void, c_int, c_char, c_ulong, c_uint, c_float, c_double, uint32_t, size_t, memset, mkfifo, fchmod, S_IRUSR, S_IWUSR, O_RDONLY, open, read, malloc, free};
+use core::str::raw::{from_c_str};
 use core::libc::funcs::posix88::unistd::{sleep};
 use core::libc::funcs::c95::string::{memcpy};
 use core::libc::{rand, RAND_MAX};
@@ -36,7 +37,7 @@ struct JackRingBuffer {
 struct ProcessArgs {
   pub out_port_ptr: *JackPort,
   pub rb_ptr: *JackRingBuffer,
-  pub chan : Chan<~str>
+  pub chan : Chan<int>
 }
 
 fn write_cstr(c: *c_char) -> () {
@@ -193,46 +194,90 @@ extern fn process_ring_buffer( frames: JackNFrames, args: *c_void ) -> c_int {
   unsafe {
     let out_port_ptr = (*(args as *ProcessArgs)).out_port_ptr; 
     let rb_ptr = (*(args as *ProcessArgs)).rb_ptr;
+    let mut offset = 0;
     let buffer = jack_port_get_buffer( out_port_ptr, frames);
-    let available_read : size_t = jack_ringbuffer_read_space(rb_ptr);
+    let bytes_available : size_t = jack_ringbuffer_read_space(rb_ptr);
+    let frames_available = bytes_available as uint/(sys::size_of::<JackDefaultAudioSample>() as uint);
+    let total_bytes_requested = frames as u64 * (sys::size_of::<JackDefaultAudioSample>() as u64);
+    let mut frames_to_be_read = 0;
 
-    //do str::as_c_str(fmt!("Available Read: %d", available_read as int)) |c_str| {
-    //  puts( c_str as *c_char );
-    //}
-
-    //do str::as_c_str(fmt!("NFrames: %d", frames as int)) |c_str| {
-    //  puts( c_str as *c_char );
-    //}
-
-    if (available_read as JackNFrames > frames) {
-      //read what you can(available_read), pad the rest with silence  
-      for uint::range(0, available_read/4 as uint) |i| {
-        let buffer_ptr = buffer as uint + (sys::size_of::<JackDefaultAudioSample>() * i);
-        let result : size_t = jack_ringbuffer_read(rb_ptr, buffer_ptr as *c_char, sys::size_of::<JackDefaultAudioSample>() as u64);
-
-        if (result  != sys::size_of::<JackDefaultAudioSample>() as u64) {
-          do str::as_c_str(fmt!("Read Result: %d", result as int)) |c_str| {
-            puts( c_str as *c_char );
-          }
-          return -1 as c_int;
-        }
-      }
-      //let offset : uint = available_read as uint + 1;
-      //let buffer_ptr = buffer as uint + (sys::size_of::<JackDefaultAudioSample>() * offset );
-      //memset( buffer_ptr as *c_void, 0, (sys::size_of::<JackDefaultAudioSample>() * (frames as uint) - offset) as u64 );
+    if (frames < frames_available as u32) {
+      frames_to_be_read = frames; 
     } else {
-      //read frames worth from buffer
-      //for uint::range(0, frames as uint) |i| {
-      //  let buffer_ptr = buffer as uint + (sys::size_of::<JackDefaultAudioSample>() * i);
-      //  let result = jack_ringbuffer_read( rb_ptr, buffer_ptr as *c_char, sys::size_of::<JackDefaultAudioSample>() as u64);
-      //  if (result != frames as u64) {
-      //    return -1;
-      //  }
-      //}
+      frames_to_be_read = frames_available as u32;
     }
-    (*(args as *ProcessArgs)).chan.send(~"give me more data");
+
+    // do str::as_c_str(~"-------------------------------------------------------") |c_str| {
+    //   puts( c_str as *c_char );
+    // }
+
+    // do str::as_c_str(fmt!("Bytes requested: %d", total_bytes_requested as int)) |c_str| {
+    //   puts( c_str as *c_char );
+    // }
+
+    // do str::as_c_str(fmt!("Frames requested: %d", frames as int)) |c_str| {
+    //   puts( c_str as *c_char );
+    // }
+
+    // do str::as_c_str(fmt!("Bytes Available: %d", bytes_available as int)) |c_str| {
+    //  puts( c_str as *c_char );
+    // }
+
+    // do str::as_c_str(fmt!("frames Available: %d", frames_available as int)) |c_str| {
+    //  puts( c_str as *c_char );
+    // }
+
+    // do str::as_c_str(fmt!("memcopying over %d frames", frames_to_be_read as int)) |c_str| {
+    //  puts( c_str as *c_char );
+    // }
+
+    //copy over as much of the requested data as we have available in the ring buffer
+    for uint::range(0, frames_to_be_read as uint) |i| {
+      let buffer_ptr = buffer as uint + offset;
+
+      //(*(args as *ProcessArgs)).chan.send( *(buffer_ptr as *f32) );
+      let result : size_t = jack_ringbuffer_read(rb_ptr, buffer_ptr as *c_char, sys::size_of::<JackDefaultAudioSample>() as u64);
+
+      if (result  != sys::size_of::<JackDefaultAudioSample>() as u64) {
+        do str::as_c_str(fmt!("ERROR: bad read result: %d", result as int)) |c_str| {
+          puts( c_str as *c_char );
+        }
+        return -1 as c_int;
+      }
+
+      offset = offset + sys::size_of::<JackDefaultAudioSample>();
+    }
+
+
+    if (bytes_available < total_bytes_requested) {
+      //do str::as_c_str(fmt!("Silence: %d", (total_bytes_requested as int - bytes_available as int) )) |c_str| {
+      //  puts( c_str as *c_char );
+      //}
+      //do str::as_c_str(fmt!("Silence: %?", total_bytes_requested - bytes_available)) |c_str| {
+      //  puts( c_str as *c_char );
+      //}
+      let buffer_ptr = buffer as uint + offset;
+      memset( buffer_ptr as *c_void, 0, (total_bytes_requested - bytes_available) );
+    }
+    (*(args as *ProcessArgs)).chan.send(frames as int);
   }
   return 0 as c_int;
+}
+
+extern fn process_loud( frames : JackNFrames, args: *c_void) -> c_int {
+  unsafe {
+    let buffer = jack_port_get_buffer( (*(args as *ProcessArgs)).out_port_ptr, frames);
+    for uint::range(0, frames as uint) |i| {
+      let mut next_sample : JackDefaultAudioSample = 10000000000000.0 as JackDefaultAudioSample;
+      if (i % 4 == 0) {
+        next_sample = next_sample * -1.0;
+      }
+      let next_sample_ptr = core::ptr::addr_of(&next_sample);
+      let buffer_ptr = buffer as uint + (sys::size_of::<JackDefaultAudioSample>() * i);
+      memcpy(buffer_ptr as *c_void, (next_sample_ptr as *c_void), sys::size_of::<JackDefaultAudioSample>() as u64);
+    }
+    return 0 as c_int; 
+  }
 }
 
 extern fn process_noise( frames : JackNFrames, args: *c_void ) -> c_int {
@@ -242,20 +287,121 @@ extern fn process_noise( frames : JackNFrames, args: *c_void ) -> c_int {
       let mut next_sample : JackDefaultAudioSample = ((rand() as c_double/ (RAND_MAX as c_double) * 2.0) - 1.0) as JackDefaultAudioSample;
       let next_sample_ptr = core::ptr::addr_of(&next_sample);
       let buffer_ptr = buffer as uint + (sys::size_of::<JackDefaultAudioSample>() * i);
-
       memcpy(buffer_ptr as *c_void, (next_sample_ptr as *c_void), sys::size_of::<JackDefaultAudioSample>() as u64);
     }
     return 0 as c_int; 
   }
 }
 
-fn main() -> () {
-  do str::as_c_str(~"Rusty Jack") |client_name| {
-    let options = 0 as c_int;
-    let mut status = BoxedJackStatus { val: 0, errors: ~[] };
-    unsafe {
-      let client : *JackClient = jack_client_open(client_name, options, & status.val); 
+fn ensure_fifo_pipe() -> () {
+  unsafe {
+    let fifo = ~"/tmp/rusty-jack-in";
+    if ( PosixPath(fifo).exists()) {
+      return
+    } 
+    do str::as_c_str(fifo) |fifo_name| {
+      if (mkfifo(fifo_name,  (S_IWUSR | S_IRUSR ) as u16) != 0) {
+        fail!(fmt!("ERROR: creating named pipe"));  
+      }
+    }
+  }
+}
 
+fn read_from_fifo_clean(rb : *JackRingBuffer, pipe: c_int) -> () {
+  unsafe {
+    let mut bytes_written = 0;
+    let mut rb_available_space = jack_ringbuffer_write_space(rb);
+
+    while (rb_available_space > bytes_written) {
+      rb_available_space = (rb_available_space / 4) * 4;
+      let mut read_buffer = malloc( rb_available_space - bytes_written);
+
+      let mut bytes_available_from_pipe = read( pipe, read_buffer as *mut c_void, rb_available_space - bytes_written); 
+
+      //No data available in pipe
+      if (bytes_available_from_pipe <= 0) {
+        free(read_buffer);
+        break;
+      }
+    
+      let mut curr_ptr : *mut c_void  = read_buffer as *mut c_void;
+
+      while (bytes_available_from_pipe > 0) {
+        let next_sample : f32 =  *(curr_ptr as  *f32); 
+
+        curr_ptr = (curr_ptr as uint + 4) as *mut c_void;
+        bytes_available_from_pipe = bytes_available_from_pipe - 4;
+
+        if (next_sample > 1.0 || next_sample < -1.0 || 
+            (next_sample > -0.001 && next_sample < 0.001))
+           {
+          loop;
+        }
+
+        bytes_written = bytes_written + 4;
+        let write_result = jack_ringbuffer_write(rb, ptr::addr_of(&next_sample) as *char, 4);
+      }
+
+      free(read_buffer);
+    }
+
+  }
+}
+
+fn read_from_fifo(rb : *JackRingBuffer, pipe: c_int) -> () {
+  unsafe {
+    let write_space = jack_ringbuffer_write_space(rb);
+    if (write_space == 0) {
+      return;
+    }
+    let mut read_buffer = malloc( write_space );
+    let bytes_read = read( pipe, read_buffer as *mut c_void, write_space); 
+
+    let curr_ptr : *mut c_void  = read_buffer as *mut c_void;
+
+    for uint::range(0, bytes_read/4 as uint) |i| {
+      let mut next_sample : f32 =  *((curr_ptr as uint + 4 * i) as *f32); 
+      let mut next_sample_int : u16=  *((curr_ptr as uint + 4 * i) as *u16); 
+      io::println(fmt!("before:\t%?", next_sample ));
+      if (next_sample > 1.0 || next_sample < -1.0) {
+        next_sample = 0.0;
+      }
+      let write_result = jack_ringbuffer_write(rb, ptr::addr_of(&next_sample) as *char, 4);
+
+      //let tuna : i16 = (next_sample_int as i32 - 32768) as i1io6;
+      //next_sample = tuna as f32 / 32768.0 as f32; 
+      //let chicken = ptr::addr_of(&next_sample); 
+
+      //io::println(fmt!("after:\t%?", *(chicken as *f32)));
+      //let write_result = jack_ringbuffer_write(rb, chicken as *char, 4);
+
+      //clear exponents
+      //next_sample_int = next_sample_int & 0b1_01111111_11111111111111111111111; 
+      //let chicken = ptr::addr_of(&next_sample_int) as *c_void;
+      //io::println(fmt!("after:  %?", *(chicken as *f32)));
+      //let write_result = jack_ringbuffer_write(rb, chicken as *char, 4);
+
+      //if (next_sample > 1.0) {
+      //   next_sample = 1.0
+      //} else if (next_sample < -1.0) {
+      //    next_sample = -1.0;
+      //} 
+      //let write_result = jack_ringbuffer_write(rb, ptr::addr_of(&next_sample) as *char, 4);
+    }
+    //let write_result = jack_ringbuffer_write(rb, read_buffer as *char, bytes_read  as u64);
+    free(read_buffer);
+  }
+}
+
+fn main() -> () {
+  unsafe { 
+    do str::as_c_str(~"Rusty Jack") |client_name| {
+      let options = 0 as c_int;
+      let mut status = BoxedJackStatus { val: 0, errors: ~[] };
+
+      ensure_fifo_pipe();
+
+      let client : *JackClient = jack_client_open(client_name, options, & status.val); 
       if (ptr::is_null(client)) {
         status.parse_jack_status();
         fail!(fmt!("ERROR: connecting to server: %?", status.errors)); 
@@ -263,16 +409,16 @@ fn main() -> () {
 
       let out_port_ptr = register_output_port(client);
       let initial_size = (1024 * sys::size_of::<JackDefaultAudioSample>() as u64);
-      let rb = jack_ringbuffer_create( initial_size );
+      let rb : *JackRingBuffer = jack_ringbuffer_create( initial_size );
 
       if (jack_ringbuffer_mlock(rb) != 0) {
         fail!(~"ERROR: unable mlock ring buffer");
       }
 
-      let (main_port, main_chan): (Port<~str>, Chan<~str>) = stream();
+      let (main_port, main_chan): (Port<int>, Chan<int>) = stream();
       let process_args = ~ProcessArgs { out_port_ptr : out_port_ptr, 
-                                        rb_ptr       : rb, 
-                                        chan         : main_chan };
+        rb_ptr       : rb, 
+        chan         : main_chan };
 
       if (jack_set_process_callback(client, process_ring_buffer, ptr::addr_of(process_args) as *c_void) != 0) {
         fail!(~"ERROR: unable to set process callback");
@@ -284,26 +430,27 @@ fn main() -> () {
 
       //list_ports(client);
 
-      loop {
-        let mut available_write = jack_ringbuffer_write_space(rb);
-        while (available_write > sys::size_of::<JackDefaultAudioSample>() as u64) {
-          let mut next_sample : JackDefaultAudioSample = ((rand() as c_double/ (RAND_MAX as c_double) * 2.0) - 1.0) as JackDefaultAudioSample;
-          let next_sample_ptr = core::ptr::addr_of(&next_sample);
+      do str::as_c_str(~"/tmp/rusty-jack-in") |pipe_path| {
+        let pipe = open( pipe_path, O_RDONLY as i32, (S_IWUSR | S_IRUSR ) as i32);
+        loop {
+          read_from_fifo_clean(rb, pipe);  
+          let next_result = main_port.recv();
+          //let mut available_write = jack_ringbuffer_write_space(rb);
+          //while (available_write > sys::size_of::<JackDefaultAudioSample>() as u64) {
+          //  let mut next_sample : JackDefaultAudioSample = ((rand() as c_double/ (RAND_MAX as c_double) * 2.0) - 1.0) as JackDefaultAudioSample;
+          //  let next_sample_ptr = core::ptr::addr_of(&next_sample);
 
-          let write_result = jack_ringbuffer_write(rb, next_sample_ptr as *char, sys::size_of::<JackDefaultAudioSample>() as u64);
+          //  let write_result = jack_ringbuffer_write(rb, next_sample_ptr as *char, sys::size_of::<JackDefaultAudioSample>() as u64);
 
-          if (write_result != 4) {
-            fail!(~"ERROR: while writing to ring buffer");
-          }
-          available_write -= write_result;
-        }
-        let next_result = main_port.recv();
-        io::println(next_result);
-        //sleep(1 as c_uint);
-      } //loop
-
+          //  if (write_result != 4) {
+          //    fail!(~"ERROR: while writing to ring buffer");
+          //  }
+          //  available_write -= write_result;
+          //}
+          //let next_result = main_port.recv();
+        } //loop
+      }
     }
   }
 }
-
 
